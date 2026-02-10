82830b3a217e56965b884e91b2b1955160d678f5
nubi <sabinbasi111@gmail.com>
2026-02-10 12:08:15 +0545

    project run successful

---
+++ src/main/java/com/nubi/fws/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.nubi.fws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MainController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
