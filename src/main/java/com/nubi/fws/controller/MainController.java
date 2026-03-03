d1a81a069b2eed888930035547a78efe410c0e42
nubi <sabinbasi111@gmail.com>
2026-03-03 12:32:48 +0545

    project upto crud and pull data from database in frontpage

--- src/main/java/com/nubi/fws/controller/MainController.java
+++ src/main/java/com/nubi/fws/controller/MainController.java
@@ -1,12 +1,20 @@
 package com.nubi.fws.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.nubi.fws.service.FrontSliderService;
 @Controller
 public class MainController {
+	
+	@Autowired
+	FrontSliderService frontSliderService;
 
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("frontSliderList" , frontSliderService.getTop5ByDateDesc());
 		return "frontend/index";
 	}
 	
